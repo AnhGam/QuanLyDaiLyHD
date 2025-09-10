47be02d41d2366ebc95be8490e4e63365d26d4a1
AnhGam <guardalex8@gmail.com>
2025-09-10 15:09:47 +0700

    Better DB, Better UI

---
+++ QuanLyDaiLyHD/Views/DaiLyViews/XoaDaiLyWindow.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+using QuanLyDaiLyHD.ViewModels.DaiLyViewModels;
+namespace QuanLyDaiLyHD.Views.DaiLyViews;
+
+public partial class XoaDaiLyWindow : Popup
+{
+	public XoaDaiLyWindow()
+	{
+		InitializeComponent();
+	}
+}
